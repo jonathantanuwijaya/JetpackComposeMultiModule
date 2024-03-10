584d9245eb3187f67bdb988f8ac714ca727d88a2
Jonathan Tanuwijaya <jonathantw@andalsoftware.com>
2024-03-10 22:07:17 +0700

    feat: create Day Selector UI

--- tracker/tracker_presentation/src/main/java/com/learndev/tracker_presentation/tracker_overview/TrackerOverViewScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/learndev/tracker_presentation/tracker_overview/TrackerOverViewScreen.kt
@@ -1,6 +1,9 @@
 package com.learndev.tracker_presentation.tracker_overview
 
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
@@ -9,6 +12,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.learndev.core.utils.UiEvent
 import com.learndev.core_ui.LocalSpacing
+import com.learndev.tracker_presentation.components.DaySelector
 import com.learndev.tracker_presentation.components.NutrientHeader
 
 @Composable
@@ -26,6 +30,16 @@ fun TrackerOverviewScreen(
     ) {
         item {
             NutrientHeader(state = state)
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+            DaySelector(
+                date = state.date,
+                onPreviousDayClick = { viewModel.onEvent(TrackerOverviewEvent.OnPreviousDayClick) },
+                onNextDayClick = { viewModel.onEvent(TrackerOverviewEvent.OnNextDayClick) },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = spacing.spaceMedium)
+            )
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
         }
     }
 }
