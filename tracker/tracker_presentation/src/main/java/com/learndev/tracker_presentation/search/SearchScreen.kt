1c3879ead3632c5e62d44b7099c1e5db3982f4d1
Jonathan Tanuwijaya <jonathantw@andalsoftware.com>
2024-10-24 21:08:11 +0700

    feat: create trackablefood and search

--- tracker/tracker_presentation/src/main/java/com/learndev/tracker_presentation/search/SearchScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/learndev/tracker_presentation/search/SearchScreen.kt
@@ -1,25 +1,39 @@
 package com.learndev.tracker_presentation.search
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.learndev.core.utils.UiEvent
 import com.learndev.core_ui.LocalSpacing
+import com.learndev.tracker_domain.model.MealType
 import com.learndev.tracker_presentation.R
 import com.learndev.tracker_presentation.search.components.SearchTextField
+import com.learndev.tracker_presentation.search.components.TrackableFoodItem
+import java.time.LocalDate
 
+@RequiresApi(Build.VERSION_CODES.O)
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun SearchScreen(
@@ -69,5 +83,53 @@ fun SearchScreen(
             onSearch = { viewModel.onEvent(SearchEvent.OnSearch) },
             onFocusChanged = { viewModel.onEvent(SearchEvent.OnSearchFocusChange(it.isFocused)) },
         )
+        Spacer(modifier = Modifier.height(spacing.spaceMedium))
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            items(state.trackableFood) { food ->
+                TrackableFoodItem(
+                    trackableFoodUiState = food,
+                    onClick = {
+                        viewModel.onEvent(
+                            SearchEvent.OnToggleTrackableFood(
+                                food = food.food,
+                            )
+                        )
+                    },
+                    onAmountChange = {
+                        viewModel.onEvent(
+                            SearchEvent.OnAmountForFoodChange(
+                                food = food.food,
+                                it
+                            )
+                        )
+                    },
+                    onTrack = {
+                        viewModel.onEvent(
+                            SearchEvent.OnTrackFoodClick(
+                                food = food.food,
+                                mealType = MealType.fromString(mealName),
+                                date = LocalDate.of(year, month, dayOfMonth)
+                            )
+                        )
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
+    }
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        when {
+            state.isSearching -> CircularProgressIndicator()
+            state.trackableFood.isEmpty() -> {
+                Text(
+                    text = stringResource(R.string.no_results),
+                    style = MaterialTheme.typography.body1,
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
     }
 }
