20677866e98e47066ddb236e51250b424080cc42
Jonathan Tanuwijaya <jonathantw@andalsoftware.com>
2024-10-26 17:10:34 +0700

    feat: update target and compile sdk to 35

--- buildSrc/src/main/java/ProjectConfig.kt
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -1,8 +1,8 @@
 object ProjectConfig {
     const val appId = "com.learndev.calorytracker"
-    const val compileSdk = 34
+    const val compileSdk = 35
     const val minSdk = 26
-    const val targetSdk = 34
+    const val targetSdk = 35
     const val versionCode = 1
     const val versionName = "1.0"
 }
